6435a097c4b84d9ab2f1e9b8169ded52c453b338
PRP <padhupradheep@gmail.com>
2024-01-30 12:42:17 +0100

    updating configs for MMO-700 with UR10

--- mpo_700_ur10/launch/spawn_controllers.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_spawn_controllers_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("mpo_700", package_name="mpo_700_ur10").to_moveit_configs()
-    return generate_spawn_controllers_launch(moveit_config)
